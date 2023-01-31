8c1f54bb56ea7b188d30e716ae799099d6c46610
Joan Fàbregas <32735468+joanft@users.noreply.github.com>
2023-01-31 17:32:27 +0100

    Initial commit

---
+++ src/components/Section/Section.tsx
@@ -0,0 +1,16 @@
+import { FC, PropsWithChildren, useContext } from "react";
+import { LevelContext } from "../../context/LevelContext";
+
+export const Section: FC<PropsWithChildren<Record<never, any>>> = ({
+  children
+}) => {
+  const level = useContext(LevelContext);
+
+  return (
+    <section className="section">
+      <LevelContext.Provider value={level + 1}>
+        {children}
+      </LevelContext.Provider>
+    </section>
+  );
+};
